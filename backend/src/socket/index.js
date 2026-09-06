8dc2641b6d8b6ca2e75a6f2e934a3e527488fa8d
SyedSaifuddin045 <93501325+SyedSaifuddin045@users.noreply.github.com>
2026-09-06 19:27:13 +0530

    feat: wire rtc events and emit ice config on connect

--- backend/src/socket/index.js
+++ backend/src/socket/index.js
@@ -6,6 +6,7 @@ const createRoomHandler = require("./handlers/roomHandler");
 const createPlaybackHandler = require("./handlers/playbackHandler");
 const createWallpaperHandler = require("./handlers/wallpaperHandler");
 const createChatHandler = require("./handlers/chatHandler");
+const { createRtcHandler, resolveIceServers } = require("./handlers/rtcHandler");
 
 function createSocketServer(httpServer) {
     const io = new Server(httpServer, {
@@ -19,9 +20,11 @@ function createSocketServer(httpServer) {
     const roomHandler = createRoomHandler(io, roomService);
     const playbackHandler = createPlaybackHandler(io, roomService);
     const wallpaperHandler = createWallpaperHandler(io, roomService);
+    const rtcHandler = createRtcHandler(io, roomService);
     const chatHandler = createChatHandler(io, roomService);
 
     io.on("connection", (socket) => {
+        socket.emit(SERVER.RTC_CONFIG, { iceServers: resolveIceServers(process.env) });
         socket.on(CLIENT.ROOM_CREATE, (payload) => roomHandler.handleCreate(socket, payload));
         socket.on(CLIENT.ROOM_JOIN, (payload) => roomHandler.handleJoin(socket, payload));
         socket.on(CLIENT.ROOM_LEAVE, () => roomHandler.handleLeave(socket));
@@ -31,6 +34,10 @@ function createSocketServer(httpServer) {
         socket.on(CLIENT.PLAYBACK_SEEK, (payload) => playbackHandler.handleSeek(socket, payload));
         socket.on(CLIENT.PLAYBACK_SET_TRACK, (payload) => playbackHandler.handleSetTrack(socket, payload));
         socket.on(CLIENT.WALLPAPER_SET, (payload) => wallpaperHandler.handleSetWallpaper(socket, payload));
+        socket.on(CLIENT.RTC_MEDIA, (payload) => rtcHandler.handleMedia(socket, payload));
+        socket.on(CLIENT.RTC_OFFER, (payload) => rtcHandler.handleOffer(socket, payload));
+        socket.on(CLIENT.RTC_ANSWER, (payload) => rtcHandler.handleAnswer(socket, payload));
+        socket.on(CLIENT.RTC_ICE, (payload) => rtcHandler.handleIce(socket, payload));
         socket.on(CLIENT.CHAT_SEND, (payload) => chatHandler.handleSend(socket, payload));
 
         socket.on("disconnect", () => {
